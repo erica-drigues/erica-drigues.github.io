f362e259de5f5f9de893478f82009dfc2bfb6613
Érica Rodrigues <erica.rodrigues.games@gmail.com>
2025-01-23 04:00:53 -0300

    App-layout

--- app-layout/flutter_bootstrap.js
+++ app-layout/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"062b3a72fcbe76296ce19b02f58c03ba0cbb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1641344074"
+    serviceWorkerVersion: "3617317492"
   }
 });
